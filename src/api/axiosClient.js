a6ba4fde25cba2f6dee811ade75b40e21d427c9b
Abi Kumar <kecit21002@kingsedu.ac.in>
2026-08-14 10:36:56 +0530

    d

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
  
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://edigitalbackend.onrender.com'
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://edigitalbackend-4twu.onrender.com'
  
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
